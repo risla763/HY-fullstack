ea537adb96f96024f019ca06d59f3f3e6b664931
Maija <maija.rislakki@helsinki.fi>
2024-03-20 16:54:46 +0200

    1.5

--- osa1/part1/src/App.jsx
+++ osa1/part1/src/App.jsx
@@ -1,32 +1,33 @@
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
   return (
     <div>
       <Header course={course} />
-      <Content parts={parts} />
-      <Total text="Number of exercises" parts={parts} />
+      <Content parts={course.parts} />
+      <Total text="Number of exercises" parts={course.parts} />
     </div>
   )
 }
 
 
-const Header = (props) => {
-  console.log(props)
-  return <h1>{props.course}</h1>
+const Header = ({ course }) => {
+  return <h1>{course.name}</h1>
 }
 
  const Content = ({ parts }) => {

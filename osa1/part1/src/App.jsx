4ba34daf51b39b02e3b3a87df76da25d10b3312a
Maija <maija.rislakki@helsinki.fi>
2024-03-26 14:29:07 +0200

    1.12

--- osa1/part1/src/App.jsx
+++ osa1/part1/src/App.jsx
@@ -1,6 +1,19 @@
 import { useState } from 'react'
 
 const App = () => {
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when dianosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
+   
+
+
   // tallenna napit omaan tilaansa
   // title:
   const header = {
@@ -19,6 +32,14 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
   const [total, setTotal] = useState(0)
+  const [selected, setSelected] = useState(0)
+
+  const handleAnecdote = () => {
+    const updateSelected = Math.floor(Math.random()*8)
+    setSelected(updateSelected)
+    console.log('anecdote', selected)
+
+  }
 
   const handleGoodClick = () => {
     console.log('good before', good)
@@ -44,6 +65,8 @@ const App = () => {
 
   return (
     <div>
+      {anecdotes[selected]} <br></br>
+      <Button value={handleAnecdote} text="next anecdote"/>
       <Header header={header.title} />
       <ButtonInfo goodClick={handleGoodClick} badClick={handleBadClick} 
       neutralClick={handleNeutralClick}/>
@@ -114,6 +137,8 @@ const StatisticLine = ( {text, value} ) => {
         </tr>
   )
 }
+
+
 
 
 

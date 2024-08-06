f5105cf74c605c4c4b96e9418149c459f5abffed
Maija <maija.rislakki@helsinki.fi>
2024-08-06 22:34:59 +0300

    2.3 muutokset

--- osa2/src/App.jsx
+++ osa2/src/App.jsx
@@ -1,87 +1,63 @@
 import React, { useState } from 'react';
 
 const App = () => {
-  const courses = [
-    {
-      name: 'Half Stack application development',
-      id: 1,
-      parts: [
-        {
-          name: 'Fundamentals of React',
-          exercises: 10,
-          id: 1
-        },
-        {
-          name: 'Using props to pass data',
-          exercises: 7,
-          id: 2
-        },
-        {
-          name: 'State of a component',
-          exercises: 14,
-          id: 3
-        },
-        {
-          name: 'Redux',
-          exercises: 11,
-          id: 4
-        }
-      ]
-    }, 
-    {
-      name: 'Node.js',
-      id: 2,
-      parts: [
-        {
-          name: 'Routing',
-          exercises: 3,
-          id: 1
-        },
-        {
-          name: 'Middlewares',
-          exercises: 7,
-          id: 2
-        }
-      ]
-    }
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    id: 1,
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
 
   return (
     <div>
-      <Course courses={courses} />
+      <Course course={course} />
     </div>
   )
 }
 
-const Course = ({ courses }) => {
+const Course = ({ course }) => {
   const [total, setTotal] = useState(42);
 
-const totall = courses.reduce( (s, p) => {
-  console.log('what is happening', s, p)
+const totall = course.parts.reduce( (s, p) => {
+  console.log('what is happening',s,p)
   return s+p.exercises
 }, 0)
 
   return (
     <div>
-      <Header courses={courses}/>
-      <Content courses={courses}/>
+      <Header course={course}/>
+      <Content course={course}/>
       <h3>total of {totall} exercises</h3>
     </div>
   )
 }
 
-const Header = ({ courses }) => {
-  console.log(courses.name)
-  return <h1>{courses.name}</h1>
+const Header = ({ course }) => {
+  console.log(course.name)
+  return <h1>{course.name}</h1>
 }
 
-const Content = ({ courses }) => {
+const Content = ({ course }) => {
   return (
     <div>
-      <p>{courses.parts[0].name} {courses.parts[0].exercises}</p>
-      <p>{courses.parts[1].name} {courses.parts[1].exercises}</p>
-      <p>{courses.parts[2].name} {courses.parts[2].exercises}</p>
-      <p>{courses.parts[3].name} {courses.parts[3].exercises}</p>
+      <p>{course.parts[0].name} {course.parts[0].exercises}</p>
+      <p>{course.parts[1].name} {course.parts[1].exercises}</p>
+      <p>{course.parts[2].name} {course.parts[2].exercises}</p>
     </div>
   )
 }

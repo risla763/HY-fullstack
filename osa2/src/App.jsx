f3a39541bea93ce1821b7664a37ecd0dda3af7a8
Maija <maija.rislakki@helsinki.fi>
2024-08-07 18:52:10 +0300

    2.4

--- osa2/src/App.jsx
+++ osa2/src/App.jsx
@@ -1,42 +1,63 @@
-import React, { useState } from 'react';
-
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    id: 1,
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      },
-      {
-        name: 'Redux',
-        exercises: 11,
-        id: 4
-      }
-    ]
-  }
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
 
   return (
     <div>
-      <Course course={course} />
+      <MainHeader header="Web development curriculum"/>
+      {courses.map(course => (
+      <Course key={course.id} course={course} />
+      ))}
     </div>
   )
 }
 
+const MainHeader = ({ header }) => {
+  return <h1>{header}</h1>}
+
 const Course = ({ course }) => {
-  const [total, setTotal] = useState(42);
 
 const totall = course.parts.reduce( (s, p) => {
   console.log('what is happening',s,p)
@@ -54,7 +75,7 @@ const totall = course.parts.reduce( (s, p) => {
 
 const Header = ({ course }) => {
   console.log(course.name)
-  return <h1>{course.name}</h1>
+  return (<h2>{course.name}</h2>)
 }
 
 const Content = ({ course }) => {

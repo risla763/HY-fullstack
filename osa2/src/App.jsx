430f8aeb3214ef0dea0fd8bc1abedbac2682307f
Maija <maija.rislakki@helsinki.fi>
2024-08-09 01:20:47 +0300

    2.6

--- osa2/src/App.jsx
+++ osa2/src/App.jsx
@@ -1,61 +1,55 @@
-import React from 'react';
-import { Course, MainHeader } from './Course'; 
-
+import React, { useState } from 'react';
 
 const App = () => {
-  const courses = [
-    {
-      name: 'Half Stack application development',
-      id: 1,
-      parts: [
-        {
-          name: 'Fundamentals of React',
-          exercises: 10,
-          id: 1
-        },
-        {
-          name: 'Using props to pass data',
-          exercises: 7,
-          id: 2
-        },
-        {
-          name: 'State of a component',
-          exercises: 14,
-          id: 3
-        },
-        {
-          name: 'Redux',
-          exercises: 11,
-          id: 4
-        }
-      ]
-    }, 
-    {
-      name: 'Node.js',
-      id: 2,
-      parts: [
-        {
-          name: 'Routing',
-          exercises: 3,
-          id: 1
-        },
-        {
-          name: 'Middlewares',
-          exercises: 7,
-          id: 2
-        }
-      ]
-    }
-  ]
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+
+  const [newName, setNewPerson] = useState('')
+
+  const addPerson = (event) => {
+  event.preventDefault()
+  console.log('uusi henkilö LISÄTTY', event.target)
+
+  const personObject = {
+    name: newName,
+    id: String(persons.length + 1),
+  }
+  setPersons(persons.concat(personObject))
+  setNewPerson('')
+
+  }
+
+  const handlePersonChange = (event) => {
+    console.log(event.target.value, 'uusi nimi kentässä')
+    setNewPerson(event.target.value)
+  }
 
   return (
     <div>
-      <MainHeader header="Web development curriculum"/>
-      {courses.map(course => (
-      <Course key={course.id} course={course} />
-      ))}
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input 
+          value = {newName} 
+          onChange={handlePersonChange}/>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <ul>
+        {persons.map(person => {
+        console.log('tässä listassa nimet',person.name);
+        return  <li key={person.name}> {person.name}</li>
+        })}
+
+      </ul>
+
     </div>
   )
+
 }
 
-export default App;
+export default App

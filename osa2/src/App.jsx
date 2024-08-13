5c5fd531e3294048cfb5f234f051f783b3afe0f4
Maija <maija.rislakki@helsinki.fi>
2024-08-13 19:42:31 +0300

    2.9

--- osa2/src/App.jsx
+++ osa2/src/App.jsx
@@ -2,14 +2,26 @@ import React, { useState } from 'react';
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '102' }
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
   ]) 
 
   const [newName, setNewPerson] = useState('')
   const [newNumber,setNewNumber] = useState('')
+  const [searchPerson, setNewSearchPerson] = useState('')
+  const [showAll, setShowAll] = useState(false)
+
+  const personsToShow = showAll 
+    ? persons
+    : persons.filter(person => 
+      person.name.toLowerCase().includes(searchPerson.toLowerCase())  ||
+      person.number.includes(searchPerson))
 
   const addPerson = (event) => {
   event.preventDefault()
+
 
   const IsInList = persons.some(person => person.name === newName)
   console.log('TARKISTUS', IsInList )
@@ -40,9 +52,19 @@ const App = () => {
     setNewNumber(event.target.value)
   }
 
+
+  const handleSearchChange = (event) => {
+    setNewSearchPerson(event.target.value);
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>        
+        filter shown with<input id="searchInput" 
+        type="text " value={searchPerson}
+        onChange={handleSearchChange}/>              
+      </div>
       <form onSubmit={addPerson}>
         <div>
           name: <input 
@@ -56,7 +78,7 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <ul>
-        {persons.map(person => {
+        {personsToShow.map(person => {
         console.log('tässä listassa nimet',person.name);
         return  <li key={person.name}> {person.name} {person.number}</li>
         })}

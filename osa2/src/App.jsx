b898b675274a8ff6c5d1bb1aaec1609f7e11783c
Maija <maija.rislakki@helsinki.fi>
2024-08-14 19:02:41 +0300

    2.11

--- osa2/src/App.jsx
+++ osa2/src/App.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios'
 import FilterForm from './FilterForm';
 import AddPersonForm from './AddPersonForm';
 import ListOfPersons from './ListOfPersons';
@@ -6,12 +7,17 @@ import ListOfPersons from './ListOfPersons';
 
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ]) 
+
+  const [persons, setPersons] = useState([]) 
+
+  useEffect(() => {
+    console.log('effect')
+    axios.get('http://localhost:3002/persons').then(response => {
+      console.log('promise fulfilled', response.data)
+      setPersons(response.data)
+    }
+      )
+  }, [])
 
   const [newName, setNewPerson] = useState('')
   const [newNumber,setNewNumber] = useState('')

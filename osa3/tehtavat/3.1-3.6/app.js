c67d4bad14101d7a81961094d9944c4c6f72aae1
Maija Rislakki <maijarislakki@Maija-MacBook-Air.local>
2025-11-29 15:38:43 +0200

    3.5

--- osa3/tehtavat/3.1-3.6/app.js
+++ osa3/tehtavat/3.1-3.6/app.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 let notes = [
   {
     id: "1",
@@ -63,6 +65,34 @@ app.get('/api/info',(request, response) => {
         notes = notes.filter(note => note.id !== id)
 
         response.status(204).end()
+    })
+
+    const generateId = () => {
+        const maxId = notes.length > 0
+            ? Math.max(...notes.map(n => Number(n.id)))
+            : 0
+        return String(maxId + 1)
+        }
+    app.post('/api/persons',(request, response) => {
+        const body = request.body
+
+        if (!body.content) {
+            return response.status(400).json({error: 'content missing'
+            })
+        }
+
+        if (!body.number) {
+            return response.status(400).json({ error: 'number missing' });
+        }
+        
+        const person = {
+            id: generateId(),
+            content: body.content,
+            number: body.number,
+        }
+        
+        notes = notes.concat(person)
+        response.json(person)
     })
 
 const PORT = 3001

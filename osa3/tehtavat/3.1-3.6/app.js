751d2a2fcc894ea36b8a7e4d9c129f0aaaef10e1
Maija Rislakki <maijarislakki@Maija-MacBook-Air.local>
2025-11-30 20:38:46 +0200

    3.9

--- osa3/tehtavat/3.1-3.6/app.js
+++ osa3/tehtavat/3.1-3.6/app.js
@@ -1,8 +1,12 @@
 const express = require('express')
-const app = express()
+const cors = require('cors')
 const morgan = require('morgan')
 
+const app = express()
+
 app.use(express.json())
+
+app.use(cors())
 
 app.use(morgan(function (tokens, request, response) {
     if (request.method === 'POST'){

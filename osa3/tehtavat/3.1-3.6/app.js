55f24df229bfd49dcd23f022c416e34eab6de5f0
Maija Rislakki <maijarislakki@Maija-MacBook-Air.local>
2025-12-04 20:46:24 +0200

    3.10

--- osa3/tehtavat/3.1-3.6/app.js
+++ osa3/tehtavat/3.1-3.6/app.js
@@ -128,7 +128,7 @@ app.get('/api/info',(request, response) => {
         response.json(person)
     })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })

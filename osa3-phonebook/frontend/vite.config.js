65e4bcb970499e9bd5105eabfbd68ff2eae6b991
Maija Rislakki <maijarislakki@Maija-MacBook-Air.local>
2025-12-06 02:12:01 +0200

    lisäyksiä phonebook appiin

--- osa3-phonebook/frontend/vite.config.js
+++ osa3-phonebook/frontend/vite.config.js
@@ -4,11 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-
-sourcemap: true, 
-},
-server: {
-  port: 3000, 
-},
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3001',
+        changeOrigin: true,
+      },
+    }
+  },
 })
